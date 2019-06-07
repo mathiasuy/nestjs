a736d8261e00c169540c5291b9e8612798875384
Mathias Battistella <mathias.battistella.contractor@bbva.com>
2019-06-07 10:26:17 -0300

    notas en controlador

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,8 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  
+  //Constructor donde recibimos por injecci[on de dependencia el servicio, como si fuese un @Autowired (?)
   constructor(private readonly appService: AppService) {}
 
   @Get()
