62aeb3937bd89ae0160e4802e5f36d9eb862f356
Mathias Battistella <mathias.battistella.contractor@bbva.com>
2019-06-07 10:25:57 -0300

    notas: controladores, main

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+@Module({//declaracion del modulo, donde se cargaran los servicios y los controladores
+  imports: [],//Modulos, dependencias, diferentes componentes que usaremos en este modulo
+  controllers: [AppController],//Los controladores que forman parte de este modulo
+  providers: [AppService],//Los servicios donde se encuentran los metodos de acceso a los datos
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  //Aca se define la instancia del nest factory para crear la api
+  //Se conecta al modulo principal de la aplicacion
   const app = await NestFactory.create(AppModule);
+  //Escucha en el puerto 3000
   await app.listen(3000);
 }
 bootstrap();
